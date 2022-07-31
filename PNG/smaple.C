049772915bed5a8550116caf5e7b14248995ab22
981122 <981122@hotmail.com.tw>
2022-07-31 20:44:08 +0800

    master

--- PNG/smaple.C
+++ PNG/smaple.C
@@ -9,24 +9,24 @@
 #include "TAxis.h"
 using namespace RooFit;
 
-void Draw(TString files)
+void sample(TString files)
 {
-    TChain *chain = new TChain("B0tree");
+    TChain *chain = new TChain("variables");
     chain->Add(files);
-    RooRealVar Mbc("Mbc", "M_{bc}", 5.24, 5.30, "GeV/c^{2}");
-    // RooRealVar deltaE("deltaE", "#Delta E", -0.2, 0.2, "GeV");
+    RooRealVar Mbc("Mbc", "M_{bc}", 5.3, 5.434, "GeV/c^{2}");
+    // RooRealVar deltaE("deltaE", "#Delta E", -0.4, 0.4, "GeV");
     // RooArgSet *varset = new RooArgSet(deltaE, Mbc);
     RooArgSet *varset = new RooArgSet(Mbc);
     RooDataSet data("data", "data", chain, *varset);
     data.Print();
     
-    Mbc.setRange("full_range", 5.24, 5.30);
-    // deltaE.setRange("full_range", -0.2, 0.2);
+    Mbc.setRange("full_range", 5.25, 5.45);
+    // deltaE.setRange("full_range", -0.4, 0.4);
 
-    RooRealVar mean("mean", "de signal mean", 5.279, 5.24, 5.30);
-    RooRealVar sigma_delta1("sigma_delta1", "sigma_delta1", 0.0029, 0, 1);
-    RooRealVar alpha1("alpha1", "alpha1", 1.702,1,2);
-    RooRealVar n1("n1", "n1", 2.276,2,3);
+    RooRealVar mean("mean", "de signal mean", 5.315, 5.25, 5.45);
+    RooRealVar sigma_delta1("sigma_delta1", "sigma_delta1", 0.00348,0.00347,0.00349);
+    RooRealVar alpha1("alpha1", "alpha1", 0.616,0.615,0.617);
+    RooRealVar n1("n1", "n1", 1.027,1.026,1.028);
     RooCBShape sig1("sig1", "Signal component 1", Mbc, mean, sigma_delta1, alpha1, n1);
 
     RooFitResult *de_result = new RooFitResult("de_result", "de_result");
@@ -40,6 +40,6 @@ void Draw(TString files)
     sig1.Print("t");
     plot_Mbc->Draw("");
 
-    TString imagename = "Fitting_eegg_Mbc.png";
+    TString imagename = "Fitting_eeggg_Mbc.png";
     canvas->SaveAs(imagename);
 }
