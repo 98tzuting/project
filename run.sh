a541d52fcc181694f816d4854aeb4e5bee212d65
981122 <981122@@hotmai.com.tw>
2023-02-22 16:21:51 +0800

    0222

---
+++ run.sh
@@ -0,0 +1,4 @@
+# rsync -av tzuting@kekccproxy:/home/belle2/tzuting/newProject/SKIM/flavor_tag_test/skim_eegg_cp_Rave_IP.root .
+# rsync -av tzuting@kekccproxy:/home/belle2/tzuting/newProject/SKIM/flavor_tag_test/skim_eegg_cp_Rave_noC.root .
+# rsync -av tzuting@kekccproxy:/home/belle2/tzuting/newProject/SKIM/flavor_tag_test/skim_eegg_cp_Rave_boost.root .
+# rsync -av tzuting@kekccproxy:/home/belle2/tzuting/newProject/SKIM/flavor_tag_test/skim_eegg_cp_Rave_tube.root .
