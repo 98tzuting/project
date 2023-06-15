9cc05a65a163e565230e4e8edefcf44c6ce8bc40
981122 <981122@@hotmai.com.tw>
2023-06-15 15:52:20 +0800

    0615

---
+++ run.zsh
@@ -0,0 +1,4 @@
+rsync -av tzuting@kekccproxy:/home/belle2/tzuting/newProject/SKIM_data/skim_eegg_0320.root .
+rsync -av tzuting@kekccproxy:/home/belle2/tzuting/newProject/SKIM_data/skim_eeppp_0320.root .
+rsync -av tzuting@kekccproxy:/home/belle2/tzuting/newProject/SKIM_data/skim_uugg_0320.root .
+rsync -av tzuting@kekccproxy:/home/belle2/tzuting/newProject/SKIM_data/skim_uuppp_0320.root .
